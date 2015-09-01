0b3a6349401b5d9fba2e0262c2d7d4a94dc9b492
eduardo.lemos <eduardo.lemos@datasus.gov.br>
2015-09-01 17:06:17 -0300

    crud angular

--- js/angular/controller/contatoController.js
+++ js/angular/controller/contatoController.js
@@ -1,36 +1,36 @@
-var app = angular.module('app',[]);
+var app = angular.module('app', []);
 
-app.controller('contatoController', ['$scope','$http', function($scope, $http) {
+app.controller('contatoController', ['$scope', '$http', function ($scope, $http) {
 
-	$scope.contato = criaContato();
-	
-	$scope.msg = "";
-	
-	$scope.msgok = false;
-	
-	$scope.msgnok = false;
-	
-	$scope.enviaEmail = function() {
+    $scope.contato = criaContato();
 
-		$http.post('http://rest-ws.elasticbeanstalk.com/enviarEmail', $scope.contato)
-		.success(
-			function(data) {
-				$scope.msgok = true;
-				$scope.msg = 'Email Enviado.'
-			})
-		.error(function() {
-			$scope.msgnok = true;
-			$scope.msg = 'Descuple não foi possivel enviar a mensagem, favor entrar em contato pelo email: educlemos@gmail.com'
-		})
-	}
-	
+    $scope.msg = "";
+
+    $scope.msgok = false;
+
+    $scope.msgnok = false;
+
+    $scope.enviaEmail = function () {
+
+        $http.post('http://rest-ws.elasticbeanstalk.com/enviarEmail', $scope.contato)
+            .success(
+                function (data) {
+                    $scope.msgok = true;
+                    $scope.msg = 'Email Enviado.'
+                })
+            .error(function () {
+                $scope.msgnok = true;
+                $scope.msg = 'Descuple não foi possivel enviar a mensagem, favor entrar em contato pelo email: educlemos@gmail.com'
+            })
+    }
+
 }]);
 
-function criaContato(){
-	return {
-		nome: "",
-		email: "",
-		assunto: "",
-		texto: ""
-	}
+function criaContato() {
+    return {
+        nome: "",
+        email: "",
+        assunto: "",
+        texto: ""
+    }
 }
